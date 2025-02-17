997b526f3f56e8e3372e973660e5fa4f9c108a5d
eduardo <educcarnn@gmail.com>
2025-02-17 07:52:37 -0300

    fix: update for format correct

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const logFilePath = path.join(__dirname, 'Rastreador_Pacotes_Recebidos.log');
 
 const hexToDecimal = (hex) => parseInt(hex, 16);
 
-const parseCoordinate = (hex) => hex ? (hexToDecimal(hex) / 1800000).toFixed(6) : "N/A";
+const parseCoordinate = (hex) => {
+    const decimalValue = hexToDecimal(hex);
+    return (decimalValue / 1800000).toFixed(6);
+};
 
 const alarmCodes = {
     "00": "normal",
@@ -16,6 +19,7 @@ const alarmCodes = {
     "FE": "acc_on",
     "FF": "acc_off"
 };
+
 
 const processLogFile = (filePath) => {
     try {
@@ -31,10 +35,11 @@ const processLogFile = (filePath) => {
     }
 };
 
+
 const parsePacket = (packet) => {
-    const data = packet.replace(/\s+/g, '');
+    const data = packet.replace(/\s+/g, ''); 
     if (!data.startsWith('7878')) return null; 
-    
+
     const protocolNumber = data.slice(6, 8);
     const isLocationPacket = protocolNumber === '12';
     const isLoginPacket = protocolNumber === '01';
@@ -48,18 +53,14 @@ const parsePacket = (packet) => {
         latitudeHemisferio: isLocationPacket ? ((parseInt(data.slice(34, 36), 16) & 0x02) ? 'N' : 'S') : "N/A",
         longitudeHemisferio: isLocationPacket ? ((parseInt(data.slice(34, 36), 16) & 0x08) ? 'E' : 'W') : "N/A",
         speed: isLocationPacket ? hexToDecimal(data.slice(36, 38)) : "N/A",
+        imei: isLoginPacket ? data.slice(8, 24) : "N/A",
         data: isLocationPacket ? `20${hexToDecimal(data.slice(8, 10))}-${hexToDecimal(data.slice(10, 12))}-${hexToDecimal(data.slice(12, 14))} ` +
               `${hexToDecimal(data.slice(14, 16))}:${hexToDecimal(data.slice(16, 18))}:${hexToDecimal(data.slice(18, 20))}` : "N/A",
-        alarm: isAlarmPacket ? (alarmCodes[data.slice(8, 10)] || "tracker") : "N/A",
-        acc: isLocationPacket ? ((parseInt(data.slice(34, 36), 16) & 0x02) ? 'on' : 'off') : "N/A",
+        alarm: isAlarmPacket ? (alarmCodes[data.slice(10, 12)] || "tracker") : "N/A",
+        acc: isLocationPacket ? ((parseInt(data.slice(34, 36), 16) & 0x04) ? 'on' : 'off') : "N/A",
         direcao: isLocationPacket ? hexToDecimal(data.slice(38, 42)) : "N/A",
-        nivelBateria: isHeartbeatPacket ? `${hexToDecimal(data.slice(8, 10)) * 20}%` : "N/A",
-        imei: isLoginPacket ? data.slice(8, 24) : "N/A",
-        serialNumber: isLoginPacket ? data.slice(24, 28) : "N/A",
-        crc: isLoginPacket ? data.slice(28, 32) : "N/A"
+        nivelBateria: isHeartbeatPacket ? `${hexToDecimal(data.slice(10, 12)) * 20}%` : "N/A"
     };
 };
 
 processLogFile(logFilePath);
-
-
